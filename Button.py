02f77e12c1894ca4b158833288bf23099c31c64d
Allison <shkodra.allison@gmail.com>
2025-02-01 15:37:29 -0500

    Added an active state to the buttons

--- Button.py
+++ Button.py
@@ -3,7 +3,15 @@ import pygame
 #Button class
 # -- to add buttons on the screen and evaluate actions on the buttons --
 class Button():
-    def __init__(self, x, y, img, scale):
+
+
+    def __init__(self, x, y, img_idle, img_active, img, scale):
+        self.x = x
+        self.y = y
+        self.img_idle = img_idle
+        self.img_active = img_active
+        self.scale = scale
+
         width = img.get_width()
         height = img.get_height()
         self.img = pygame.transform.scale(img, (int(width * scale), int(height * scale)))
@@ -16,11 +24,16 @@ class Button():
         pos = pygame.mouse.get_pos() #To get the mouse's position
 
         if self.rect.collidepoint(pos): #if the mouse is over the button
+            
+            self.__init__(self.x, self.y, self.img_idle, self.img_active, self.img_active, self.scale)
+
             if (pygame.mouse.get_pressed()[0] == 1 and self.clicked == False): #if button is clicked
                 self.clicked = True
                 action = True
-        
-        if (pygame.mouse.get_pressed()[0] == 0): # if button is clicked
+        else:
+            self.__init__(self.x, self.y, self.img_idle, self.img_active, self.img_idle, self.scale)
+
+        if (pygame.mouse.get_pressed()[0] == 0): # if button is not clicked
             self.clicked = False
 
         surface.blit(self.img, (self.rect.x, self.rect.y))
--- MainMenu.py
+++ MainMenu.py
@@ -6,6 +6,7 @@ pygame.init()
 #Set the window size
 SCREEN_WIDTH = 600 
 SCREEN_HEIGHT = 600
+BTN_SCALE = 2 #Scale of the buttons
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Main Menu")
@@ -20,10 +21,14 @@ TEXT_COLOR = (255, 255, 255) #text color
 #Load the button images
 start_img = pygame.image.load("images/start.png").convert_alpha()
 exit_img = pygame.image.load("images/exit.png").convert_alpha()
+start_active_img = pygame.image.load("images/start_hover.png").convert_alpha()
+exit_active_img = pygame.image.load("images/exit_hover.png").convert_alpha()
+
+
 
 #Create the buttons
-start_btn = Button.Button(((SCREEN_WIDTH/2)-(start_img.get_width())), ((SCREEN_HEIGHT/2)-(start_img.get_height())), start_img, 2)
-exit_btn = Button.Button(((SCREEN_WIDTH/2)-(exit_img.get_width())), ((SCREEN_HEIGHT/2)-(exit_img.get_height()))+100, exit_img, 2)
+start_btn = Button.Button(((SCREEN_WIDTH/2)-(start_img.get_width())), ((SCREEN_HEIGHT/2)-(start_img.get_height())), start_img, start_active_img, start_img, BTN_SCALE)
+exit_btn = Button.Button(((SCREEN_WIDTH/2)-(exit_img.get_width())), ((SCREEN_HEIGHT/2)-(exit_img.get_height()))+100, exit_img, exit_active_img, exit_img, BTN_SCALE)
 
 #For when we need to display text
 def draw_text(text, font, text_color, posx, posy):
