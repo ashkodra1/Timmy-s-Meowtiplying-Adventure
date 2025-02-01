7f5a90280b05d114308b3f9ff582ef7902e26566
alexyegh16 <ayeghiayan@outlook.com>
2025-02-01 12:46:35 -0500

    timer and exception

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import random;
+import time;
 points=0 #total number of points of the user
 gameContinue=True
 
 print("Welcome to the game!")
+start=time.time()
 
 while gameContinue:
     n1=random.randint(0,12)
     n2=random.randint(0,12)
     correct=n1*n2
 
-    print(n1,"x",n2, "=")
-    answer=input()
-    answer=int(answer)
+    try:
+        print(n1,"x",n2, "=")
+        answer=input()
+        answer=int(answer)
 
-    if answer==correct:
-        points+=1
+        if answer==correct:
+            print("Correct!")
+            points+=1
+        else:
+            print("Incorrect. The right answer is", correct,".")
 
-    if points==10:
-        gameContinue=False
+        if points==10:
+            gameContinue=False
+            end=time.time()
+    except:
+        print("Not a valid entry!")
 
 print("Game over! You won!")
+timeTaken=int(end-start)
+# if timeTaken/60>=1:
+#     mins=
 
+print("That took", timeTaken, "seconds")
