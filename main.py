b91697459eafc95dcb3dabf5efb76bb211ed9d14
alexyegh16 <ayeghiayan@outlook.com>
2025-02-01 18:27:37 -0500

    cat moving with answers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+import random
+import time
 #from scripts.utilities import load_image, load_images
 #from scripts.player import Player
 
@@ -92,23 +94,98 @@ moving_sprites=pygame.sprite.Group()
 player = Player(100,100)
 moving_sprites.add(player)
 
+points=0 #total number of points of the user
+gameOver=False #checks to see if the game is done to display the final messages
+text_font=pygame.font.SysFont("Arial", 18, bold=True)
+userText='' #users entry
+question='' #the question asked to the user
+
+def draw_text(text, font, text_col, x, y):
+    img=font.render(text, True, text_col)
+    screen.blit(img, (x,y))
+
+
+def calculateTime(start, end):
+    #calculate time
+    timeTaken=int(end-start)
+    if timeTaken/60>=1:
+        mins=int (timeTaken/60)
+        sec=timeTaken-(mins*60)
+        message="That took "+ str(mins)+" minutes and "+str(sec)+ " seconds."
+    else:
+        message="That took "+ str(timeTaken)+ " seconds."
+    return message
+
+
+generate=True
+message=''#displays the time taken by the user
+start=time.time()
+
 while True:
+    screen.fill((0,0,0))
+    draw_text("Welcome to the game!", text_font, (255,255,255), 0, 0)
+
+    if generate:
+        n1=random.randint(0,12)
+        n2=random.randint(0,12)
+        correct=n1*n2
+        question+=str(n1)+'x'+str(n2)+'='
+        generate=False
+
+
+    draw_text(question, text_font, (255,255,255),0,20)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player.walk()
-            if event.key == pygame.K_d:
-                player.sad()
-            if event.key == pygame.K_w:
-                player.idle()
+            # if event.key == pygame.K_a:
+            #     player.walk()
+            # if event.key == pygame.K_d:
+            #     player.sad()
+            # if event.key == pygame.K_w:
+            #     player.idle()
+
+            #typing on the screen    
+            if event.key==pygame.K_BACKSPACE:
+                userText=userText[:-1]
+            elif event.key==pygame.K_RETURN:
+                generate=True
+                try:
+                    answer=int(userText)
+                    if answer==correct:
+                        #draw_text("Correct!", text_font, (255,255,255), 0, 80)
+                        points+=1
+                        player.walk()
+                    else:
+                        #draw_text(("Incorrect. The right answer is", correct,"."), text_font, (255,255,255), 0,80)
+                        player.sad()
+                    userText=''
+                    question=''
+
+                    if points==10:
+                        gameOver=True
+                        end=time.time()
+                        message=calculateTime(start,end)
+                        generate=False
+                except:
+                    #draw_text("Invalid answer.", text_font, (255,255,255), 0, 80)
+                    userText=''
+                    question=''
+            else:
+                userText+=event.unicode
         #if event.type == pygame.KEYUP:
          #   player.idle()
 
-    screen.fill((0,0,0))
+    if gameOver:
+        draw_text("Game over! You won!", text_font, (255,255,255), 0, 80)
+        draw_text(message, text_font, (255,255,255), 0, 100)
+
+    draw_text(userText, text_font, (255,255,255),0,40)
+
     moving_sprites.draw(screen)
     moving_sprites.update(0.1,3)
     pygame.display.flip()
     clock.tick(60)
+pygame.quit()
