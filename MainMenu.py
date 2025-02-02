2ef3aefa7e02a2d67fbb76285ebddf63c866b730
Allison <shkodra.allison@gmail.com>
2025-02-01 21:53:03 -0500

    Added logo on the main menu

--- MainMenu.py
+++ MainMenu.py
@@ -25,12 +25,14 @@ exit_img = pygame.image.load("images/exit.png").convert_alpha()
 start_active_img = pygame.image.load("images/start_hover.png").convert_alpha()
 exit_active_img = pygame.image.load("images/exit_hover.png").convert_alpha()
 background_img = pygame.image.load("images/background.png").convert_alpha()
+img = pygame.image.load("images/logo.png").convert_alpha()
+logo_img = pygame.transform.scale(img, (int(img.get_width() * 1.5), int(img.get_height() * 1.5)))
 
 
 
 #Create the buttons
-start_btn = Button.Button(((SCREEN_WIDTH/2)-(start_img.get_width()/1.5)), ((SCREEN_HEIGHT/8)-(start_img.get_height()/2)), start_img, start_active_img, start_img, BTN_SCALE_START)
-exit_btn = Button.Button(((SCREEN_WIDTH/2)-(exit_img.get_width()/1.5)), ((SCREEN_HEIGHT/8)-(exit_img.get_height()/2))+75, exit_img, exit_active_img, exit_img, BTN_SCALE_EXIT)
+start_btn = Button.Button(((SCREEN_WIDTH/2)-(start_img.get_width()/1.5)), ((SCREEN_HEIGHT/2)-(start_img.get_height()/2)), start_img, start_active_img, start_img, BTN_SCALE_START)
+exit_btn = Button.Button(((SCREEN_WIDTH/2)-(exit_img.get_width()/1.5)), ((SCREEN_HEIGHT/2)-(exit_img.get_height()/2))+75, exit_img, exit_active_img, exit_img, BTN_SCALE_EXIT)
 
 #For when we need to display text
 def draw_text(text, font, text_color, posx, posy):
@@ -44,9 +46,13 @@ draw_buttons = True
 
 while run:
     screen.blit(background_img, (0,0))
+    
+
     if(draw_buttons==False):
         draw_text("THIS IS THE GAME", font, TEXT_COLOR, 0, 0)
         #--- DISPLAY THE ACTUAL GAME HERE ---
+    else:
+        screen.blit(logo_img, (((SCREEN_WIDTH/2)-(logo_img.get_width()/2)),25))
 
     if (draw_buttons and start_btn.draw(screen)): #if the start button is clicked
         draw_buttons = False #Stop drawing the buttons
